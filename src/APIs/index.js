65fcb3e5ee3c1fc53839aa26eb1c10ae07bf7262
Your Name <you@example.com>
2023-01-18 14:58:04 +0700

    Fix minor bugs

---
+++ src/APIs/index.js
@@ -0,0 +1,17 @@
+import { getHomeAPI, getChartHomeAPI } from './homeAPI';
+import { getSingerAPI } from './singerAPI';
+import { getTop100API } from './top100API';
+import { getNewReleaseAPI } from './newReleaseAPI';
+import { getDetailSongAPI, getSongAPI, getDetailPlaylistAPI, searchAPI } from './musicAPI';
+
+export {
+    getHomeAPI,
+    getChartHomeAPI,
+    getTop100API,
+    getNewReleaseAPI,
+    getSingerAPI,
+    getDetailSongAPI,
+    getSongAPI,
+    getDetailPlaylistAPI,
+    searchAPI
+};
